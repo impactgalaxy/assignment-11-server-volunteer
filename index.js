0140e2d67f0154c3b2894afa70a5202fd9ea05de
impactgalaxy <traderglow@gmail.com>
2024-05-10 00:40:03 +0600

    project initial setup done

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// express middleware
+app.use(cors());
+app.use(express.json())
