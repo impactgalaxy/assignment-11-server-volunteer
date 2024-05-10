9a61a8bc4bfc64e4db16bd4c6938e4d107786f3f
impactgalaxy <traderglow@gmail.com>
2024-05-11 00:48:25 +0600

    create post for volunteer

--- index.js
+++ index.js
@@ -28,14 +28,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const volunteerDatabase = client.db("assignment_11").collection("volunteer");
+
+        // create post for volunteer
+        app.post("/volunteer", async (req, res) => {
+            const volunteerData = req.body;
+            const result = await volunteerDatabase.insertOne(volunteerData);
+            res.send(result);
+        })
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
