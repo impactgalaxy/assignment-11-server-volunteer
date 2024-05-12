936c8b712a6e679b4ac0ebe1863cbb8a60a2d97e
impactgalaxy <traderglow@gmail.com>
2024-05-12 15:53:16 +0600

    post for be volunteer

--- index.js
+++ index.js
@@ -29,13 +29,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        const volunteerDatabase = client.db("assignment_11").collection("volunteer");
+        const databaseCollection_1 = client.db("assignment_11").collection("volunteer");
+        const databaseCollection_2 = client.db("assignment_11").collection("becomeVolunteer");
 
         // create post for volunteer
 
         app.post("/volunteer", async (req, res) => {
             const volunteerData = req.body;
-            const result = await volunteerDatabase.insertOne(volunteerData);
+            const result = await databaseCollection_1.insertOne(volunteerData);
             res.send(result);
         });
 
@@ -56,14 +57,50 @@ async function run() {
             if (filter) {
                 query = { category: regEx }
             }
-            const result = await volunteerDatabase.find(query, options).skip(pageNumber * size).limit(size).toArray();
+            const result = await databaseCollection_1.find(query, options).skip(pageNumber * size).limit(size).toArray();
             res.send(result);
         });
+        app.get("/count", async (req, res) => {
+            const query = req.query.filter;
+            const regEx = { $regex: query, $options: "i" };
+            let options = {};
+            if (query) {
+                options = { category: regEx }
+            }
+            const result = await databaseCollection_1.countDocuments(options);
+            res.send({ count: result });
+        })
 
         app.get("/volunteer/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
-            const result = await volunteerDatabase.findOne(query);
+            const result = await databaseCollection_1.findOne(query);
+            res.send(result);
+        });
+        app.post("/becomeVolunteer", async (req, res) => {
+            const id = req.query.id;
+            const docs = req.body;
+            const condition = [
+                { $set: { numberOfVolunteer: { $toInt: "$numberOfVolunteer" } } },
+                { $set: { numberOfVolunteer: { $subtract: ["$numberOfVolunteer", 1] } } }
+            ]
+            let filter = {};
+            if (id) {
+                filter = { _id: new ObjectId(id) }
+            }
+            await databaseCollection_1.findOneAndUpdate(filter, condition)
+
+            const result = await databaseCollection_2.insertOne(docs);
+            res.send(result);
+        });
+
+        app.get("/becomeVolunteer", async (req, res) => {
+            const email = req.query.email;
+            let query = {};
+            if (email) {
+                query = { volunteerEmail: email }
+            }
+            const result = await databaseCollection_2.find(query).toArray();
             res.send(result);
         })
         // Connect the client to the server	(optional starting in v4.7)
